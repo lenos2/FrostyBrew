098f61c1cc9ae8cfb5547295d81fe6642ec501f5
Leo Moyo <nickdacrack@gmail.com>
2024-09-05 21:39:39 +0200

    Added active class to side bar

--- FrostyBrewPortal/src/components/SideBar.jsx
+++ FrostyBrewPortal/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { LinkContainer } from 'react-router-bootstrap'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,6 +8,9 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const SideBar = () => {
 
+    const location = useLocation().pathname;
+    console.log("Location", location);
+
     return (
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
 
@@ -15,24 +18,24 @@ const SideBar = () => {
             <div class="sidebar-sticky">
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <Link to="/" className='nav-link active'>
+                        <Link to="/" className={'nav-link ' + (location == "/" ? "active" : "")}>
                             <span data-feather="home"></span>
                             Dashboard <span class="sr-only">(current)</span>
                         </Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="products" className='nav-link'>
+                        <Link to="products" className={'nav-link ' + (location.includes("products") ? "active" : "")}>
                             <span data-feather="shopping-cart"></span>
                             Products</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="Recipes" className='nav-link'>
+                        <Link to="recipes" className={'nav-link ' + (location.includes("recipes") ? "active" : "")}>
                             <span data-feather="shopping-cart"></span>
                             Recipes</Link>
 
                     </li>
                     <li class="nav-item">
-                        <Link to="orders" className='nav-link'>
+                        <Link to="orders" className={'nav-link ' + (location.includes("orders") ? "active" : "")}>
                             <span data-feather="shopping-cart"></span>
                             Orders</Link>
 
